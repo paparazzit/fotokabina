b38aed9798e8c47fca78a648a7a6ad1562d8fab9
paparazzit <paparazzit@fgmail.com>
2022-10-13 11:33:29 +0200

    observer

--- js/main.js
+++ js/main.js
@@ -6,9 +6,43 @@ function setUp() {
 }
 function circlesHeights() {
 	circle_dims.forEach((dims) => {
-		console.log(dims.offsetWidth);
 		dims.style.height = `${dims.offsetWidth}px`;
 	});
 }
 
 window.addEventListener("load", setUp);
+
+// Proba
+window.addEventListener("dblclick", slider);
+let item = document.querySelector(".item");
+
+function slider() {
+	item.classList.toggle("active");
+}
+
+// INTERSECTION OBSERVER
+
+let options = {
+	root: null,
+	rootMargin: "-350px -50px ",
+	// threshold: 0.05,
+};
+
+let inView = document.querySelectorAll(".inView");
+
+let observer = new IntersectionObserver(beTouching, options);
+
+inView.forEach((element) => {
+	observer.observe(element);
+});
+
+function beTouching(entries) {
+	entries.forEach((entry) => {
+		if (entry.isIntersecting) {
+			console.log(entry);
+			entry.target.classList.add("active");
+		} else {
+			entry.target.classList.remove("active");
+		}
+	});
+}
---
+++ js/slider.js
@@ -0,0 +1,1 @@
+function Slider(prop) {}
